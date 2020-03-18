f9e0a18f0f851cc8c58e788e69c6fe9695a3b281
MiYazJE <ruben.saizserrano@gmail.com>
2020-03-18 14:43:42 +0100

    isValid user method changed to post

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -1,22 +1,8 @@
 const model = require('../model/pm_manager.model');
 
-// Obtener todos los puntuaciones
 exports.isValidUser = async (req, res) => {
 	const connection = await model.getConnection();
-    connection.connect(async err => {
-        if (!err) {
-            let sql = `SELECT * FROM Alumnos`;
-            connection.query(sql, (error, results, fields) => {
-                if (error) {
-                    return console.error(error.message);
-                }
-                res.send(results);
-            });
-        } else {
-            console.log(
-                "Database connectie niet gelukt!  : " +
-                    JSON.stringify(err, undefined, 2)
-            );
-        }
-    });
+    const user = req.body;
+    console.log(user);
+    res.send(user);
 };
