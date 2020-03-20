49c9dd594b7256c42b3a0ea7ac3260c6492a512d
MiYazJE <ruben.saizserrano@gmail.com>
2020-03-20 20:10:36 +0100

    changed bcryt to bcryptjs

--- app/model/pm_manager.model.js
+++ app/model/pm_manager.model.js
@@ -1,47 +1,44 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-
-module.exports.getConnection=function(){
-
-    const mysql = require('mysql');
+const getConnection = () => {
+    const mysql = require("mysql");
 
     let config = {
-        host     : 'localhost',
-        user     : 'pm_manager',
-        password : 'Covid-19',
-        database : 'pm_projects',
-        insecureAuth : true
+        host: "localhost",
+        user: "pm_manager",
+        password: "Covid-19",
+        database: "pm_projects",
+        insecureAuth: true
     };
 
     return mysql.createConnection(config);
-}
+};
 
-
-module.exports.getEnCrypted = async function(str){
-    const bcrypt = require('bcrypt');
-
-    const saltRounds = await bcrypt.genSaltSync(13);
-
+const getEnCrypted = (str) => {
+    const saltRounds = bcrypt.genSaltSync(13);
     return bcrypt.hashSync(str, saltRounds);
-}
+};
 
-module.exports.isCorrectPassword = async function(myPlaintextPassword,hash){
-    const bcrypt = require('bcrypt');
-
+const isCorrectPassword = (myPlaintextPassword, hash) => {
     return bcrypt.compareSync(myPlaintextPassword, hash);
+};
 
-}
-
-module.exports.createWebToken = (id) => {
-    const jwt = require('jsonwebtoken');
+const createWebToken = id => {
     /* It's Magic! */
-    return jwt.sign( id  , 'dawdiw', {
+    return jwt.sign(id, "dawdiw", {
         expiresIn: 60 * 60 * 24
     });
-}
+};
 
-module.exports.verifyToken = (token) => {
-    const jwt = require('jsonwebtoken');
-    return jwt.verify(token, 'dawdiw', function(err, decoded) {
-        return (decoded);
-        });
+const verifyToken = token => {
+    return jwt.verify(token, "dawdiw", (err, decoded) => decoded);
+};
+
+module.exports = {
+    getConnection,
+    getEnCrypted,
+    isCorrectPassword,
+    createWebToken,
+    verifyToken
 }
