fd46029f716f5aa0ca865c28e6e8bbefea2a8a11
Adrián Núñez <anunezpedroche@gmail.com>
2020-03-26 13:10:15 +0100

    Actualizando

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -20,54 +20,6 @@ exports.findById = async id => {
   return false;
 };
 
-exports.isValidUser = async (req, res) => {
-  const connection = model.getConnection();
-
-  connection.connect(async err => {
-    if (err) {
-      return console.error(
-        "Database connectie niet gelukt!  : " +
-          JSON.stringify(err, undefined, 2)
-      );
-    }
-
-    const { nickname, password } = req.body;
-
-    let sql = `SELECT * FROM Usuarios WHERE nickname ='${nickname}'`;
-    connection.query(sql, (error, results) => {
-      if (error) {
-        return console.error(error.message);
-      }
-
-      if (!results.length) {
-        return res.send({ msg: "Usuario no valido" });
-      }
-
-      let isCorrect = model.isCorrectPassword(password, results[0].password);
-
-      if (isCorrect) {
-        let webToken = model.createWebToken({
-          id: results[0].id
-        });
-
-        // Creamos el objeto usuario
-        // uno de los campos del usuario es el token
-        const user = parseUser(results);
-        user.token = webToken;
-
-        // Creamos el objeto cookie httponly
-        res.setHeader("x-auth-token", webToken);
-        res.send(user);
-
-        //return res.send(user);
-      } else {
-        console.log({ msg: "Usuario no valido" });
-        return res.send({ msg: "Usuario no valido" });
-      }
-    });
-  });
-};
-
 exports.isValidToken = (req, res) => {
   const data = model.verifyToken(req.body.token);
 
@@ -75,16 +27,6 @@ exports.isValidToken = (req, res) => {
     auth: data !== undefined,
     data
   });
-};
-
-exports.whoAmI = async (req, res) => {
-
-  const connection = await model.getConnection();
-  const [rows] = await connection.execute('SELECT id, nombre, apellidos, nickname, avatar, admin FROM `Usuarios` WHERE `id` = ?',[req.params.id]);
-  if (rows.length){ 
-    const user=parseUser(rows);
-    res.send(user);
-  }
 };
 
 const parseUser = results => {
