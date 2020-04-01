f7b462e2194eccbc1eff750e90e0f89a34f6360c
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2020-04-01 10:55:40 +0200

    Populating techs

--- db/populate-tecnologias.sql
+++ db/populate-tecnologias.sql
@@ -1,15 +1,20 @@
 use pm_projects;
 
--- admin -> admin
--- jagaroc -> ancla
-
-
 TRUNCATE TABLE Tecnologias;
 
 INSERT INTO Tecnologias VALUES (NULL,"JavaScript","ECMAScript v6","js.png","6.0");
-INSERT INTO Tecnologias VALUES (NULL,"PHP","Powerfull Home Page","php7.png","7.0");
-INSERT INTO Tecnologias VALUES (NULL,"PHP","Powerfull Home Page","php52.png","5.2");
+INSERT INTO Tecnologias VALUES (NULL,"PHP","Powerfull Home Page","php.png","7.0");
+INSERT INTO Tecnologias VALUES (NULL,"PHP","Powerfull Home Page","php.png","5.2");
 INSERT INTO Tecnologias VALUES (NULL,"Python2","Coding Language From Guido Van Rossum","python27.png","2.7");
+INSERT INTO Tecnologias VALUES (NULL,"NodeJS","Node Server","nodejs.png","10");
+INSERT INTO Tecnologias VALUES (NULL,"React","JS Framework","react.png","16.+");
+INSERT INTO Tecnologias VALUES (NULL,"Vue","JS Framework","vue.png","2.6.+");
+INSERT INTO Tecnologias VALUES (NULL,"Angular","JS Framework","angular.png","1.7.+");
+INSERT INTO Tecnologias VALUES (NULL,"Laravel","PHP Framework","laravel.png","7.4.+");
+INSERT INTO Tecnologias VALUES (NULL,"MongoDB","NOSQL Database","mongo.png","4.0.8+");
+INSERT INTO Tecnologias VALUES (NULL,"MariaDB","Relational Database","mariadb.png","10.4.12");
+INSERT INTO Tecnologias VALUES (NULL,"MySQL","Relational Database","mysql.png","8.+");
+INSERT INTO Tecnologias VALUES (NULL,"MySQL","Relational Database","mysql.png","5.+");
 
 
 
