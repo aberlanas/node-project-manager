9e357acf0aa7db558db7f7e05445f9710acb06c9
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2020-04-06 08:02:16 +0200

    Updated database

--- db/populate-usuarios.sql
+++ db/populate-usuarios.sql
@@ -4,12 +4,26 @@ use pm_projects;
 -- jagaroc -> ancla
 
 TRUNCATE TABLE Usuarios;
+-- Admin
 INSERT INTO Usuarios VALUES (NULL,"admin","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","admin@ieslasenia.org","Administrador","De Todo",1,"admin.png");
-INSERT INTO Usuarios VALUES (NULL,"jagaroc","$2b$13$F6K3EoRuYyu5XWz04UXzh.TnXu4gVxkMCywC1hDbCd/h6qRbQwTCu","javier@ieslasenia.org","Javier","Garcia Ortega",0,"javi.png");
-INSERT INTO Usuarios VALUES (NULL,"raul.gomez","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","raul.gomez@ieslasenia.org","Raúl","Gomez Lopez",0,"raul.png");
-INSERT INTO Usuarios VALUES (NULL,"ruben.saiz","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","ruben.saiz@ieslasenia.org","Rubén","Saiz Serrano",0,"daw.png");
-INSERT INTO Usuarios VALUES (NULL,"adrian.nuñez","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","adrian.nunyez@ieslasenia.org","Adrián","Nuñez Pedroche",0,"daw.png");
+
+-- Teachers
+INSERT INTO Usuarios VALUES (NULL,"aberlanas","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","angel.berlanas@ieslasenia.org","Angel","Berlanas Vicente",0,"admin.png");
+INSERT INTO Usuarios VALUES (NULL,"diegarta","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","diego.garcia@ieslasenia.org","Diego","Garcia Tarin",0,"admin.png");
+
+-- Students
+INSERT INTO Usuarios VALUES (NULL,"victor","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","victor.alvarez@ieslasenia.org","Victor","Alvarez Camps",0,"daw.png");
 INSERT INTO Usuarios VALUES (NULL,"carlos.anguix","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","carlos.anguix@ieslasenia.org","Carlos","Anguix Genoves",0,"daw.png");
-INSERT INTO Usuarios VALUES (NULL,"aberlanas","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","angel.berlanas@ieslasenia.org","Angel","Berlanas",0,"admin.png");
+INSERT INTO Usuarios VALUES (NULL,"toni","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","toni.bermejo@ieslasenia.org","Toni","Bermejo Valenzuela",0,"daw.png");
+INSERT INTO Usuarios VALUES (NULL,"miguel","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","miguel.cantos@ieslasenia.org","Miguel","Cantos Gil",0,"daw.png");
+INSERT INTO Usuarios VALUES (NULL,"jorge","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","jorge.rolando@ieslasenia.org","Jorge Rolando","Duran Villavicencio",0,"daw.png");
+INSERT INTO Usuarios VALUES (NULL,"jagaroc","$2b$13$F6K3EoRuYyu5XWz04UXzh.TnXu4gVxkMCywC1hDbCd/h6qRbQwTCu","javier@ieslasenia.org","Javier","Garcia Ortega",0,"javi.png");
+INSERT INTO Usuarios VALUES (NULL,"jonatan","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","jonatan.gil@ieslasenia.org","Jonatan","Gil Galera",0,"daw.png");
+INSERT INTO Usuarios VALUES (NULL,"raul.gomez","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","raul.gomez@ieslasenia.org","Raúl","Gomez Lopez",0,"raul.png");
+INSERT INTO Usuarios VALUES (NULL,"adrian.nuñez","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","adrian.nunyez@ieslasenia.org","Adrián","Nuñez Pedroche",0,"daw.png");
+INSERT INTO Usuarios VALUES (NULL,"ruben.saiz","$2b$13$U7kld3mcyhdw4ge1XQi5jePCNWsPLf9XHevKwFQkCwayaqowREYsa","ruben.saiz@ieslasenia.org","Rubén","Saiz Serrano",0,"daw.png");
+
+
+
 
 
